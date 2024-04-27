9a92a7db3a84af602ad7a1b2f1d6def7fcfbaa40
AlexanderTorkar <alexandertorkar@gmail.com>
2024-04-27 20:46:42 +0200

    andrat projekt

---
+++ Hänga Gubbe.py
@@ -0,0 +1,6 @@
+# Hänga Gubbe
+
+import random
+ 
+# Lista Av Sporter
+Sporter = ["fotboll", "golf", "basket", "handboll", "cricket", "rugby", "badminton", "bowling", "tennis", "boxning", "dart", "hockey", "squash", "volleyboll", "taekwondo"]
