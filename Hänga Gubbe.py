d0c11d6b32e0118332e1d98caf1ea7fe17d78553
AlexanderTorkar <alexandertorkar@gmail.com>
2024-04-27 21:02:06 +0200

    while slinga

--- Hänga Gubbe.py
+++ Hänga Gubbe.py
@@ -4,3 +4,9 @@ import random
  
 # Lista Av Sporter
 Sporter = ["fotboll", "golf", "basket", "handboll", "cricket", "rugby", "badminton", "bowling", "tennis", "boxning", "dart", "hockey", "squash", "volleyboll", "taekwondo"]
+
+ord = random.choice(Sporter)
+
+omgångar = 6
+
+while omgångar > 6: 
